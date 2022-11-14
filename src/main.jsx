1022b630977ee2db6b97b7736a7a599503de811f
ivesal1 <mahdijafari.v@gmail.com>
2022-11-14 17:18:00 +0330

    project created

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './assets/styles/index.css'
+import './assets/styles/reset.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
+
+
+
+
+
